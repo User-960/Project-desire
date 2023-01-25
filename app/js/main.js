bcb3f5d651ea8ec393b0358bd44176de00690fc7
Ivan <ivanvolodin960@gmail.com>
2023-01-25 15:58:31 +0300

    Issue 06.03.01 - Add articles in blog.html

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,11 @@ $(function () {
     slidesToScroll: 8
   });
 
+  $('.article-slider__box').slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="images/arrow-slider-left.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="images/arrow-slider-right.svg" alt="arrow right"></button>'
+  });
+
   const mixer = mixitup('.gallery__inner', {
     load: {
       filter: '.living'
