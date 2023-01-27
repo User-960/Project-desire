8aea097bb757f13da27629134fdc2f78752f2181
Ivan <ivanvolodin960@gmail.com>
2023-01-28 01:46:58 +0300

    Issue 08.02.01 - Add adaptive for element header

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,10 @@ $(function () {
     $('.rightside-menu').addClass('rightside-menu--close')
   });
 
+  $('.header__btn-menu').on('click', function () {
+    $('.menu').toggleClass('menu--open')
+  });
+
   $('.top__slider').slick({
     arrows: false,
     dots: true,
